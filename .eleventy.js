4c71a733991244e64d181cae5ed8d5c0b3f6bf9a
jess-miz <JessicaRmisra@gmail.com>
2023-08-04 15:26:19 -0500

    added js solution to month, fixed show title

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,20 @@
 const { DateTime } = require('luxon');
 
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
     
+    
     eleventyConfig.addFilter('postDate', (dateObj) =>
     {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     })
+
+    eleventyConfig.addShortcode("month", () => `${new Date().toLocaleString('default', {month: 'long'})}`);
+
+  
     
 
     return {
